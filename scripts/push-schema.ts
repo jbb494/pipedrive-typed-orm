147f1f8e27e2dfd4405bd31382658c3b5ae404cf
Joan Bellavista <jbb494@gmail.com>
2024-01-01 11:53:38 +0100

    feat: :hammer: Adding push schema to package.json

---
+++ scripts/push-schema.ts
@@ -0,0 +1,16 @@
+import { pushToPipedrive } from "src/push-schema";
+
+const main = async () => {
+  const schemaPath = process.env.SCHEMA_PATH;
+  if (!schemaPath)
+    throw new Error("Environmenet variable SCHEMA_PATH is not defined");
+
+  const schema = await import(schemaPath);
+  const exportedSchema = Object.values(schema)[0];
+
+  const result = await pushToPipedrive(exportedSchema);
+
+  if (!result.ok) throw new Error("Error pushing schema to Pipedrive");
+};
+
+main();
